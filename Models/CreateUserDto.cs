c847a15b88bebebb9fa1dbc5aa46ddce3d85a25b
AshwinKaliraj <ashwin.kaliraj@psiog.com>
2025-10-19 01:55:16 +0530

    connected

---
+++ Models/CreateUserDto.cs
@@ -0,0 +1,13 @@
+﻿public class CreateUserDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }  // Required for creating users
+    public DateTime DateOfBirth { get; set; }
+    public int Age { get; set; }
+    public string Designation { get; set; }
+    public string Department { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public string? ImageUrl { get; set; }
+}
